251718a6598da46cba2c9b089e55ab34a65cb03c
farassauliapr <farasauliapr@gmail.com>
2026-04-03 22:09:10 +0700

    first commit: menambahkan file project dan .gitignore

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TP_MODUL6_103022400024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
